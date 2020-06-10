96df38e3550ff23ab4718faa5ece37aea73d978f
yosuke.aramaki <gnx.vw903@gmail.com>
2020-06-10 11:37:01 +0900

    add user create page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  root 'major_tasks#index'
   get 'major_tasks/index'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  resources :users do
+    collection do
+    end
+  end
 
   resources :major_tasks do
     collection do
