d2767c77cc60fc9da0fb94a395a5206365d0855a
yosuke.aramaki <gnx.vw903@gmail.com>
2020-06-10 10:55:58 +0900

    add login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render 'new'
+    end
   end
 
   def destroy
+    log_out if logged_in?
+    flash[:notice] = 'ログアウトしました'
+    redirect_to root_url
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :major_tasks
 end
