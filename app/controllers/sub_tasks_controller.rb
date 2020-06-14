bfba565bfdbf0d0887b46cb1637f5700e54260b6
yosuke.aramaki <gnx.vw903@gmail.com>
2020-06-15 08:03:47 +0900

    create sub task form

--- app/controllers/sub_tasks_controller.rb
+++ app/controllers/sub_tasks_controller.rb
@@ -5,17 +5,17 @@ class SubTasksController < ApplicationController
   end
 
   def create 
-    render plain: params.inspect
-    # @sub_task = SubTask.new(sub_task_params)
-    # @sub_task.major_task_id = params[:major_task_id]
-    # if @sub_task.save
-    #   redirect_to root_path()
-    # end
+    # render plain: params.inspect
+    @sub_task = SubTask.new(sub_task_params)
+    @sub_task.major_task_id = params[:major_task_id]
+    if @sub_task.save
+      redirect_to root_path()
+    end
   end
 
   private
   def sub_task_params
-    params.require(:major_task).permit(:text, :start_date, :end_date, :time_duration)
+    params.require(:sub_task).permit(:text, :start_date, :end_date, :time_duration)
   end
 
 end
