288ad72c820d17e2827dabeefaa72d107bff027a
Yosuke-Aramaki <gnx.vw903@gmail.com>
2020-06-23 20:19:18 +0900

    update sum time of sub task when detail task created

--- app/controllers/detail_tasks_controller.rb
+++ app/controllers/detail_tasks_controller.rb
@@ -7,8 +7,14 @@ class DetailTasksController < ApplicationController
   def create 
     @detail_task = DetailTask.new(detail_task_params)
     @detail_task.sub_task_id = params[:sub_task_id]
+    @detail_task.date = Date.today
     @sub_task = SubTask.find(params[:sub_task_id])
-    
+    if @sub_task.time_duration.nil? 
+      @sub_task.update(time_duration: params[:detail_task][:time_duration])
+    else
+      sum = @sub_task.time_duration + params[:detail_task][:time_duration].to_f
+      @sub_task.update(time_duration: sum)
+    end
     if @detail_task.save
       redirect_to root_path()
     end
@@ -36,7 +42,7 @@ class DetailTasksController < ApplicationController
 
   private
   def detail_task_params
-    params.require(:detail_task).permit(:text, :start_date, :date, :time_duration)
+    params.require(:detail_task).permit(:text, :date, :time_duration)
   end
 
 end
