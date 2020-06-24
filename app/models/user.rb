5c2563242abec7db743475d12225d99f76181aff
Yosuke-Aramaki <gnx.vw903@gmail.com>
2020-06-24 11:00:25 +0900

    account validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }
+  before_validation { email.downcase! }
   has_secure_password
+  validates :password, presence: true
   has_many :major_tasks
   has_many :sub_tasks
 end
